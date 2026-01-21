3098095ea4162266ff44c06e39858da097067c17
Bulbulla-cmd <mkhairnar0506@gmail.com>
2026-01-21 15:02:07 +0530

    Tuning thresholds to test on dataset

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,12 +2,12 @@ from embedding.clip_encoder import get_embedding
 from embedding.phash_encoder import get_phash
 from similarity.similarity_metrics import cosine_similarity
 
-phash_threshold = 5        # Hamming distance
+phash_threshold = 15        # Hamming distance
 clip_threshold = 0.8       #cosine similarity
 
 def test_pair(img1_path, img2_path):
-    img1_path = "data/raw/original/img1.jpg"
-    img2_path = "data/raw/modified/img1_crop.jpg"
+    #img1_path = "data/raw/original/img1.jpg"
+    #img2_path = "data/raw/modified/img1_crop.jpg"
 
     phash1 = get_phash(img1_path)
     phash2 = get_phash(img2_path)
@@ -34,13 +34,13 @@ def test_pair(img1_path, img2_path):
 
 def main():
     test_pair(
-        "data/raw/original/img1.jpg",
-        "data/raw/modified/img1_crop.jpg"
+        "data/raw/original/img14.jpg",
+        "data/raw/modified/img1.jpg"
     )
 
     test_pair(
-        "data/raw/original/img2.jpg",
-        "data/raw/original/img3.jpg"
+        "data/raw/original/img24.jpg",
+        "data/raw/original/img32.jpg"
     )
 
 if __name__ == "__main__":
