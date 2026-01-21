b991a6ee26f918881ddb898e9c73f6682381dfda
Bulbulla-cmd <mkhairnar0506@gmail.com>
2026-01-21 11:57:56 +0530

    Fixed error caused in pipeline due to phash

--- src/embedding/phash_encoder.py
+++ src/embedding/phash_encoder.py
@@ -1,7 +1,7 @@
-from PIL import image
+from PIL import Image
 import imagehash
 
-def compute_phash(image_path:str): #computes phash of image to a 64 bit perceptual hash
+def get_phash(image_path:str): #computes phash of image to a 64 bit perceptual hash
     image = Image.open(image_path).convert("RGB") 
     phash = imagehash.phash(image) #computes perceptual hash using DCT-based method
 
