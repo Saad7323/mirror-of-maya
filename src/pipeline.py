dc197d1236b2733e541c772faed9c12b522f68eb
mdsaad7323 <mdsaad7323@gmail.com>
2026-01-24 03:16:05 +0530

    Altered pipeline.py for UI

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,25 +4,39 @@ from src.similarity.similarity_metrics import cosine_similarity
 
 
 class DuplicateDetector:
-    def __init__(self, phash_threshold=15, clip_threshold=0.8):
+    def __init__(self, mode="hybrid", phash_threshold=18, clip_threshold=0.30):
+        self.mode = mode
         self.phash_threshold = phash_threshold
         self.clip_threshold = clip_threshold
 
     def compare(self, img1_path, img2_path):
-        # Step 1: p-Hash Filtering
+        #pHash 
+        if self.mode == "phash":
+            phash1 = get_phash(img1_path)
+            phash2 = get_phash(img2_path)
+            distance = phash1 - phash2 #hamming distance
+            decision = distance <= self.phash_threshold
+            return float(distance), decision
+
+        #CLIP
+        if self.mode == "clip":
+            emb1 = get_embedding(img1_path)
+            emb2 = get_embedding(img2_path)
+            score = cosine_similarity(emb1, emb2)
+            decision = score >= self.clip_threshold
+            return score, decision
+
+        #Hybrd
         phash1 = get_phash(img1_path)
         phash2 = get_phash(img2_path)
+        distance = phash1 - phash2 #hamming distance
 
-        hamming_distance = phash1 - phash2
+        if distance > self.phash_threshold:
+            return 0.0, False #rejected early
 
-        if hamming_distance > self.phash_threshold:
-            return 0.0, False  # Rejected Early
-
-        # Step 2: CLIP Similarity
         emb1 = get_embedding(img1_path)
         emb2 = get_embedding(img2_path)
-
         score = cosine_similarity(emb1, emb2)
         decision = score >= self.clip_threshold
-
         return score, decision
+
